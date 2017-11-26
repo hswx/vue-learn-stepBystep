3b9e1154fa65fc7ba70667ea415f3b45f0b08972
wangpeng <597084762@qq.com>
2017-11-26 16:42:35 +0800

    根据api文档看了一下config的全局配置内容，把生命周期的钩子函数表示的意义注释在钩子函数的名字后面了

--- src/core/config.js
+++ src/core/config.js
@@ -8,8 +8,12 @@ import {
 
 import { LIFECYCLE_HOOKS } from 'shared/constants'
 
+/**
+ * 该js文件定义一个用户的全局配置类型，生成一个默认的配置对象出去
+ * 相关配置属性的用途可以在api文档中找到https://cn.vuejs.org/v2/api/#全局配置
+ */
 export type Config = {
-  // user
+  // user 用户配置属性
   optionMergeStrategies: { [key: string]: Function };
   silent: boolean;
   productionTip: boolean;
@@ -20,7 +24,7 @@ export type Config = {
   ignoredElements: Array<string>;
   keyCodes: { [key: string]: number | Array<number> };
 
-  // platform
+  // platform 平台配置属性
   isReservedTag: (x?: string) => boolean;
   isReservedAttr: (x?: string) => boolean;
   parsePlatformTagName: (x: string) => string;
@@ -28,92 +32,112 @@ export type Config = {
   getTagNamespace: (x?: string) => string | void;
   mustUseProp: (tag: string, type: ?string, name: string) => boolean;
 
-  // legacy
+  // legacy 遗留使用的属性
   _lifecycleHooks: Array<string>;
 };
 
 export default ({
   /**
    * Option merge strategies (used in core/util/options)
+   * 自定义合并策略的选项。用于core/util/options中
    */
   optionMergeStrategies: Object.create(null),
 
   /**
    * Whether to suppress warnings.
+   * 是否屏蔽日志警告
    */
   silent: false,
 
   /**
    * Show production mode tip message on boot?
+   * 启动时是否显示生产模式的提示信息
    */
   productionTip: process.env.NODE_ENV !== 'production',
 
   /**
    * Whether to enable devtools
+   * 是否使用调试工具
    */
   devtools: process.env.NODE_ENV !== 'production',
 
   /**
    * Whether to record perf
+   * 是否记录性能信息
    */
   performance: false,
 
   /**
    * Error handler for watcher errors
+   * 监测到错误的处理程序
    */
   errorHandler: null,
 
   /**
    * Warn handler for watcher warns
+   * 监测到警告的处理程序
    */
   warnHandler: null,
 
   /**
    * Ignore certain custom elements
+   * 忽略某些自定义元素
    */
   ignoredElements: [],
 
   /**
    * Custom user key aliases for v-on
+   * v-on的用户自定义键名
    */
   keyCodes: Object.create(null),
 
   /**
    * Check if a tag is reserved so that it cannot be registered as a
    * component. This is platform-dependent and may be overwritten.
+   * 检查一个标签是否是保留标签来判断它能否注册成一个组件。
+   * 这是一个平台相关的方法，可以被重写。
    */
   isReservedTag: no,
 
   /**
    * Check if an attribute is reserved so that it cannot be used as a component
    * prop. This is platform-dependent and may be overwritten.
+   * 检查一个属性是否是保留属性来判断它能否注册成一个组件的属性。
+   * 这是一个平台相关的方法，可以被重写。
    */
   isReservedAttr: no,
 
   /**
    * Check if a tag is an unknown element.
    * Platform-dependent.
+   * 检查一个标签是否是位置元素
+   * 平台相关
    */
   isUnknownElement: no,
 
   /**
    * Get the namespace of an element
+   * 获取元素的命名空间，即元素的类型
    */
   getTagNamespace: noop,
 
   /**
    * Parse the real tag name for the specific platform.
+   * 解析特殊平台中标签真实的标签名
    */
   parsePlatformTagName: identity,
 
   /**
    * Check if an attribute must be bound using property, e.g. value
    * Platform-dependent.
+   * 检查某个属性是否必须被用属性绑定，比如value等
+   * 平台相关
    */
   mustUseProp: no,
 
   /**
    * Exposed for legacy reasons
+   * 由于遗留原因暴露的生命周期钩子列表
    */
   _lifecycleHooks: LIFECYCLE_HOOKS
 }: Config)
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -9,14 +9,14 @@ export const ASSET_TYPES = [
 
 //生命周期钩子
 export const LIFECYCLE_HOOKS = [
-  'beforeCreate',
-  'created',
-  'beforeMount',
-  'mounted',
-  'beforeUpdate',
-  'updated',
-  'beforeDestroy',
-  'destroyed',
-  'activated',
-  'deactivated'
+  'beforeCreate', // 在实例初始化之后，数据观测 (data observer) 和 event/watcher 事件配置之前被调用。
+  'created', // 在实例创建完成后被立即调用。在这一步，实例已完成以下的配置：数据观测 (data observer)，属性和方法的运算，watch/event 事件回调。然而，挂载阶段还没开始，$el 属性目前不可见。
+  'beforeMount', // 在挂载开始之前被调用：相关的 render 函数首次被调用。
+  'mounted', // 在挂载开始之前被调用：相关的 render 函数首次被调用。
+  'beforeUpdate', // 数据更新时调用，发生在虚拟 DOM 重新渲染和打补丁之前。
+  'updated', // 由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子。
+  'beforeDestroy', // 实例销毁之前调用。在这一步，实例仍然完全可用。
+  'destroyed', // Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，所有的事件监听器会被移除，所有的子实例也会被销毁。
+  'activated', // keep-alive 组件激活时调用。
+  'deactivated' // keep-alive 组件停用时调用。
 ]
